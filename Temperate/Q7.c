8fc1f1b18c3d3da7a62093faf45bc72dd671044d
hetsheta <132735556+hetsheta@users.noreply.github.com>
2023-06-12 20:54:43 +0530

    Add files via upload

---
+++ Temperate/Q7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n,ft=0,st=1,nt;
+    
+    nt=ft+st;
+    
+    printf("Enter the number of terms:");
+    scanf("%d",&n);
+    
+    printf("Fibonacci series : %d, %d, ",ft, st);
+    
+    for(i=3 ; i<=n ; ++i)
+    {
+        printf("%d, ",nt);
+    ft=st;
+    st=nt;
+    nt = ft + st;
+    }
+    
+    return 0;
+}
